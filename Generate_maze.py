b4f6e8a64c3497d80faf489e1db4d9e55679f5a8
Sebastian L <sluraschi@fi.uba.ar>
2019-05-05 13:18:25 -0300

    fix typos

--- Generate_maze.py
+++ Generate_maze.py
@@ -7,23 +7,20 @@ if len(sys.argv) != 4:
 	print("Error")
 	exit()
 
-if((int(sys.argv[2]) <= 0) | (int(sys.argv[3]) <= 0)):
+if (int(sys.argv[2]) <= 0) | (int(sys.argv[3]) <= 0):
 	print("Error indice negativo")
 	exit()
 
 m = Maze.Maze(int(sys.argv[3]), int(sys.argv[2]))
 
-if (str(sys.argv[1]) == "dfs"):
+if str(sys.argv[1]) == "dfs":
 	m.build_dfs()
-
-if (str(sys.argv[1]) == "dyv"):
+elif str(sys.argv[1]) == "dyv":
 	m.join_grid()
 	m.recursive_generation()
-
 else:
 	print("Error funcion no reconocida")
 	exit()
 
-file = open("maze.txt","w")
-
+file = open("maze.txt", "w")
 file.write(str(m))
--- Solve_maze.py
+++ Solve_maze.py
@@ -2,12 +2,12 @@ import Maze
 import sys
 
 if __name__ == '__main__':
-	if(len(sys.argv) != 2):
+	if len(sys.argv) != 2:
 		print("Error numero de argumentos")
 		exit()
 
-	file = open(sys.argv[1],"r")
-	m = Maze.Maze(1,1)
+	file = open(sys.argv[1], "r")
+	m = Maze.Maze(1, 1)
 	text = file.readlines()
 	m.grid.get_grid(text)
 	m.redefine_start_end()
