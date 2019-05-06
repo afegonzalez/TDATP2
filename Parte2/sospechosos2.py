e7d8e1bec7d2760e16c9746ead73399070f185ba
yagosalvador <yagosalvador@gmail.com>
2019-05-06 13:40:32 -0300

    Update

--- Parte2/sospechosos2.py
+++ Parte2/sospechosos2.py
@@ -11,9 +11,7 @@ class GeneradorSospechosos:
 
         print(len(self.listaIngresos))
 
-        self.listaIngresos.sort(key=lambda x: x.hora_salida, reverse=False)
-
-        lista_entradas = []
+        lista_entradas = self.listaIngresos
 
         lista_entradas = (self.listaIngresos).copy()
 
@@ -21,10 +19,6 @@ class GeneradorSospechosos:
 
         lista_salidas = (self.listaIngresos).copy()
 
-
-
-
-  # hay que crear metodos sort by entrance y exit
         lista_auxiliar = []
 
 
@@ -34,19 +28,26 @@ class GeneradorSospechosos:
             tiempo_ilicito = primer_complice.tiempototal
 
             for candidato in lista_salidas:
-                if not (candidato.hora_salida - tiempo_inicio < 40):  # descarta candidato si el tiempo es menor a 40
-                    if candidato.hora_salida - tiempo_inicio < tiempo_ilicito:
-                        tiempo_ilicito = candidato.hora_salida - tiempo_inicio
-                        lista_auxiliar.append(candidato)
-                        primero_en_salir = candidato
+                if(candidato.hora_salida - tiempo_inicio < 40):  # descarta candidato y todos los que le sigan si el tiempo es menor a 40
+                    break
+                    
+                if candidato.hora_salida - tiempo_inicio < tiempo_ilicito:
+                    tiempo_ilicito = candidato.hora_salida - tiempo_inicio
+                    lista_auxiliar.append(candidato)
+                    primero_en_salir = candidato
 
-                    if candidato.hora_salida - tiempo_inicio >= tiempo_ilicito:
-                        lista_auxiliar.append(candidato)
+                elif candidato.hora_salida - tiempo_inicio >= tiempo_ilicito:
+                    lista_auxiliar.append(candidato)
 
-                    if candidato.nombre == primer_complice.nombre:  # pasar a la siguiente iteracion
-                        lista_auxiliar = []
-                        break
+                elif candidato.nombre == primer_complice.nombre:  #si el candidato es el primer complice, significa que salio y entro -> pasar a la siguiente iteracion
+                    lista_auxiliar = []
+                    break
+
+                elif candidato 
+
+                #elif que analiza cuando estan todos dentro
 
             if len(lista_auxiliar) >= 5 & len(lista_auxiliar) <= 10:
                 lista_de_sospechosos.append(lista_auxiliar)
 
+        for set_sospechosos in lista_de_sospechosos:
